94d6a0d820a7b969f27dd94d899453974d5a6ba4
ben <benja.lev@gmail.com>
2020-12-22 10:15:12 +0100

    - easyadmin: ville update

--- src/Controller/Admin/VilleCrudController.php
+++ src/Controller/Admin/VilleCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\Ville;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class VilleCrudController extends AbstractCrudController
 {
@@ -12,14 +14,11 @@ class VilleCrudController extends AbstractCrudController
         return Ville::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom'),
         ];
     }
-    */
 }
